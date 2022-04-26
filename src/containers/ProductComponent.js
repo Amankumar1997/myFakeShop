88fc8c49d733a9a7e3225dd2615a02f51bcd2c24
Aman <jainwalaman@gmail.com>
2022-04-26 11:19:12 +0530

    update redux store and show product on screen

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,35 +1,31 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
-function ProductComponent() {
-  
-  const products=useSelector((state)=>state.allProducts.products)
-// so products in the form of arry so use map
-const renderList=products.map((product)=>{
-  // destructuring 
-  const {id,title,category,price,image}=product;
-  return(
-    <div className='four column wide' key={id}>
-    <div className='ui link cards'>
-      <div className='card'>
-        <div className='image'>
-          <img src={image} alt={title}/>
-        </div>
-          <div className='content'>
-            <div className='header'>{title}</div>
-            <div className='meta price'>$ {price}</div>
-            <div className='meta'>{category}</div>
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const renderList = products.map((product) => {
+    const { id, title, image, price, category } = product;
+    return (
+      <div className="four wide column" key={id}>
+        <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">$ {price}</div>
+                <div className="meta">{category}</div>
+              </div>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
-    </div>
-  );
-})
+    );
+  });
+  return <>{renderList}</>;
+};
 
-
-
-  return (
-  <> {renderList} </>
-  );
-}
-
-export default ProductComponent
+export default ProductComponent;
